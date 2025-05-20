85b78c8250b2727dfb344b483e6c89e9442044f8
jazelly <xzha4350@gmail.com>
2025-05-20 11:59:32 +0000

    fix docker

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an axios instance
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api',
+  baseURL:'/api',
   headers: {
     'Content-Type': 'application/json',
   },
